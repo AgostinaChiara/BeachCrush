8eef29f59c3a5f42f79669475e0490171cbf0297
Agostina <agosschiara25@gmail.com>
2019-04-14 23:07:59 -0300

    Version 1

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+    public Text movesText, scoreText;
+    private int moveCounter;
+    private int score;
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public int MoveCounter
+    {
+        get { return moveCounter; }
+        set
+        {
+            moveCounter = value;
+            movesText.text = "Moves: " + moveCounter;
+
+            if(moveCounter <= 0)
+            {
+                moveCounter = 0;
+                StartCoroutine(GameOver());
+            }
+        }
+    }
+
+    public static GUIManager sharedInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        score = 0;
+        moveCounter = 30;
+        movesText.text = "Moves: " + moveCounter;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    private IEnumerator GameOver()
+    {
+        yield return new WaitUntil(() => !BoardManager.sharedInstance.isShifting);
+        yield return new WaitForSeconds(0.25f);
+        //TODO: invocar una pantalla diseñada para el Game Over
+    }
+}
